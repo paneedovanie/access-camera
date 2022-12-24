9b6ebc573eb6756899a5225664645a8e3d04be8d
paneedovanie <paneedovanie@yahoo.com>
2022-12-24 10:27:53 +0800

    Fixed camera page

--- src/server.js
+++ src/server.js
@@ -21,6 +21,10 @@ app.use(cors());
 
 app.use("/", express.static(path.join(public)));
 
+app.get("/camera", function (req, res) {
+  res.sendFile(path.join(__dirname + "/public", "camera.html"));
+});
+
 app.get("/*", function (req, res) {
   res.sendFile(path.join(public, "index.html"));
 });
@@ -74,16 +78,28 @@ const runPuppeteer = async () => {
     args: ["--use-fake-ui-for-media-stream"],
   });
   const page = await browser.newPage();
+  page.on("console", (message) => console.log(message.text()));
   //   const contentHtml = fs.readFileSync(__dirname + "/index.html", "utf8");
   await page.goto("http://localhost:5000/camera");
+  // const cdp = await page.target().createCDPSession();
+  // await cdp.send("Network.enable");
+  // await cdp.send("Page.enable");
+
+  // const printResponse = (response) => console.log("response: ", response);
+
+  // cdp.on("Network.webSocketFrameReceived", printResponse); // Fired when WebSocket message is received.
+  // cdp.on("Network.webSocketFrameSent", printResponse); // Fir
+
+  // const data = await page.evaluate(() => document.querySelector("*").outerHTML);
+
+  // console.log(data);
 
   await page.exposeFunction("log", console.log);
 
-  await page.evaluate(async () => {
-    // use window.readfile to read contents of a file
-    // const content = await window.readfile("/etc/hosts");
-    window.log(window.navigator);
-  });
+  // await page.evaluate(async () => {
+  //   // use window.readfile to read contents of a file
+  //   // const content = await window.readfile("/etc/hosts");
+  // });
   // video session starts without prompt
   // return browser.close();
 };
